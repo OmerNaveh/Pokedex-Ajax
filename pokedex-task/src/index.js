153df7edb20e30a837465314c8cb02164d8e88fb
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-10-14 20:15:10 +0300

    added types buttons

--- pokedex-task/src/index.js
+++ pokedex-task/src/index.js
@@ -1,12 +1,13 @@
 //elements
-const inputById= document.getElementById("inputById");
-const searchByIdButton = document.getElementById("searchByIdButton");
-const byIdName = document.getElementById("byIdName")
-const byIdHeight = document.getElementById("byIdHeight")
-const byIdWeight = document.getElementById("byIdWeight")
-const byIdImg = document.getElementById("byIdImg")
-const idSearchHeading = document.getElementById("idSearchHeading")
-
+const inputById= document.getElementById('inputById');
+const searchByIdButton = document.getElementById('searchByIdButton');
+const byIdName = document.getElementById('byIdName')
+const byIdHeight = document.getElementById('byIdHeight')
+const byIdWeight = document.getElementById('byIdWeight')
+const byIdImg = document.getElementById('byIdImg')
+const idSearchHeading = document.getElementById('idSearchHeading')
+const pokemonDet = document.getElementById('pokemonDet');
+const Types = document.getElementById('Types') 
 
 const getPokemonById = async (Id) => {
     try {
@@ -24,9 +25,10 @@ const changeDomDescById = async (id) =>{
         byIdName.innerText ='name: ' + data.name;
         byIdHeight.innerText ='height: ' + data.height;
         byIdWeight.innerText = 'weight: ' + data.weight;
-        byIdImg.src= data.sprites["front_default"];
+        byIdImg.src= data.sprites['front_default'];
         byIdImg.addEventListener('mouseover', (e) => changepostion(data.sprites))
         errorHandler(); //reset this label after change in case of an error
+        addPokemonTypes(data.types);
         return 
     } catch (error) {
         return
@@ -34,18 +36,40 @@ const changeDomDescById = async (id) =>{
 }
 const changepostion = (data) =>{
     byIdImg.src = data['back_default'];
-    byIdImg.addEventListener("mouseout" , () =>{
-        byIdImg.src= data["front_default"]; })
+    byIdImg.addEventListener('mouseout' , () =>{
+        byIdImg.src= data['front_default']; })
 }
 const errorHandler = (error) =>{
     if(error){
-        idSearchHeading.innerText = 'This Id does not exist in Pokedex' //in case of an error
+        idSearchHeading.innerText = 'This Pokemon does not exist in Pokedex' //in case of an error
         idSearchHeading.style.color = 'red'
+        pokemonDet.style.visibility = 'hidden';
     }
     else{
-        idSearchHeading.innerText = "Pokemon Info:" //reset this label after change in case of an error
+        idSearchHeading.innerText = 'Pokemon Info:' //reset this label after change in case of an error
         idSearchHeading.style.color = 'black';
+        pokemonDet.style.visibility = 'visible'
+    }
+}
+
+const addPokemonTypes = (typesArr) =>{
+    deleteExistingTypes(); //delete already created typed to avoid duplication
+    for(let type of typesArr){
+        const button = document.createElement('button');
+        button.innerText= type.type.name;
+        button.classList.add('pokeTypes');
+        Types.append(button);
+    }
+}
+
+const deleteExistingTypes = () =>{
+    const buttons = document.getElementsByClassName('pokeTypes');
+    for(let button of buttons){
+        if(buttons.length>0){
+        button.remove()
+        deleteExistingTypes(); //recursion because without it the first type always remains
+        }
     }
 }
 //event listeners
-searchByIdButton.addEventListener("click",(e)=> changeDomDescById(inputById.value))
+searchByIdButton.addEventListener('click',(e)=> changeDomDescById(inputById.value))
